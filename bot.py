3f5c1afdbe2e7f5558f9230f2a9e5692161f763a
AbhishekRana23 <40822125+AbhishekRana23@users.noreply.github.com>
2020-01-04 19:02:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@ import os
 import logging
 import re
 import httplib
+import httplib2
 import urllib2
 import json
 import html
@@ -20,14 +21,6 @@ from telegram.ext import MessageHandler
 from telegram.ext import Filters
 from telegram.ext import CommandHandler
 from telegram import ChatAction
-from mimetypes import guess_type
-from apiclient.discovery import build
-from apiclient.http import MediaFileUpload
-from apiclient.errors import ResumableUploadError
-from oauth2client.client import OAuth2WebServerFlow
-from oauth2client.file import Storage
-from oauth2client import file, client, tools
-import httplib2
 import validators
 import modules.download as download
 import modules.download_audio as download_audio
@@ -41,22 +34,17 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 size = "empty"
 filename="none"
-userfile="none"
-usr_id = int(Creds.USER_ID)
 TOKEN = Creds.TOKEN
 ADMIN_MODULE = True
 try:
 	import private.extra_funcs as extras
 except ImportError:
 	ADMIN_MODULE = False
-	
+
 @run_async
 def start(bot,update):
-	myid = int(update.message.chat_id)
-	if myid == usr_id :
-		bot.send_message(chat_id=update.message.chat_id, text=Text.GREET_USER.format(update.message.from_user.first_name))
-	else :
-		bot.send_message(chat_id=update.message.chat_id, text=Text.NOT_USER.format(update.message.from_user.first_name))
+	bot.send_message(chat_id=update.message.chat_id, text=Text.GREET_USER.format(update.message.from_user.first_name))
+
 @run_async
 def help(bot,update):
 	bot.send_message(chat_id=update.message.chat_id, text=Text.HELP, parse_mode=telegram.ParseMode.HTML)
@@ -71,95 +59,100 @@ def donate(bot,update):
 
 @run_async
 def start_bot(bot, update):
-	myid = int(update.message.chat_id)
-	if myid != usr_id :
-		bot.send_message(chat_id=update.message.chat_id, text=Text.NOT_USER.format(update.message.from_user.first_name))
-	else :
-		msg=update.message.text
-		msg=str(msg)
-		user_id=update.message.from_user.id
-		user_id =str(user_id)
-		if "|" in msg:
-			user_cmd, url = msg.split("|")
-			user_cmd = user_cmd.strip()
-			user_cmd = user_cmd.lower()
-			url = url.strip()
-		else:
-			user_cmd = None
-			url = msg
-		#site = requests.get(url)
-		#if site.status_code == 200:
-		#site = httplib.HTTPConnection(url)
-		#site.request("HEAD", '')
-		#if site.getresponse().status == 200:
-		if validators.url(url) or download.is_downloadable(url):
-			sent_message = bot.send_message(chat_id=update.message.chat_id, text=Text.PROCESSING)
-			if user_cmd:
-				if(user_cmd == "video"):
-					filename = download_video.download(url)
+	msg=update.message.text
+	msg=str(msg)
+	user_id=update.message.from_user.id
+	user_id =str(user_id)
+	if "|" in msg:
+		user_cmd, url = msg.split("|")
+		user_cmd = user_cmd.strip()
+		user_cmd = user_cmd.lower()
+		url = url.strip()
+	else:
+		user_cmd = None
+		url = msg
+	#site = requests.get(url)
+	#if site.status_code == 200:
+	#site = httplib.HTTPConnection(url)
+	#site.request("HEAD", '')
+	#if site.getresponse().status == 200:
+	sent_message = bot.send_message(chat_id=update.message.chat_id, text=Text.VERIFYING_URL)
+	time.sleep(1)
+	if validators.url(url):
+		sent_message.edit_text(Text.PROCESSING)
+		if user_cmd:
+			if(user_cmd == "video"):
+				filename = download_video.download(url)
+				if "ERROR" in filename:
+					sent_message.edit_text(Text.FAILED+filename, parse_mode=telegram.ParseMode.HTML)
+				else:
+					bot.send_chat_action(chat_id=update.message.chat_id, action=telegram.ChatAction.TYPING)
+					sent_message.edit_text(Text.UPLOADING_GD)
+					dwnld_url = upload.upload(filename)
+					size = (os.path.getsize(filename))/1048576
+					sent_message.edit_text(Text.DONE.format(filename, size, dwnld_url),parse_mode=telegram.ParseMode.HTML)
+					os.remove(filename)
+			elif(user_cmd == "audio"):
+				if("youtube" in url or "youtu" in url):
+					filename = download_audio.download(url)
 					if "ERROR" in filename:
 						sent_message.edit_text(Text.FAILED+filename, parse_mode=telegram.ParseMode.HTML)
 					else:
 						bot.send_chat_action(chat_id=update.message.chat_id, action=telegram.ChatAction.TYPING)
-						sent_message.edit_text(Text.UPLOADING_GD)
-						dwnld_url = upload.upload(filename)
-						size = (os.path.getsize(filename))/1048576
-						sent_message.edit_text(Text.DONE.format(filename, size, dwnld_url),parse_mode=telegram.ParseMode.HTML)
+						sent_message.edit_text(Text.UPLOADING_TG)
+						audio=open(filename, 'rb')
+						bot.send_audio(chat_id=update.message.chat_id, audio=audio, caption=filename.replace(".mp3",""))
+						audio.close()
 						os.remove(filename)
-				elif(user_cmd == "audio"):
-					if("youtube" in url or "youtu" in url):
-						filename = download_audio.download(url)
-						if "ERROR" in filename:
-							sent_message.edit_text(Text.FAILED+filename, parse_mode=telegram.ParseMode.HTML)
-						else:
-							bot.send_chat_action(chat_id=update.message.chat_id, action=telegram.ChatAction.TYPING)
-							sent_message.edit_text(Text.UPLOADING_TG)
-							audio=open(filename, 'rb')
-							bot.send_audio(chat_id=update.message.chat_id, audio=audio, caption=filename.replace(".mp3",""))
-							audio.close()
-							os.remove(filename)
-							sent_message.edit_text(Text.DONE)
-					else:
-						sent_message.edit_text(Text.NOT_SUPPORTED,parse_mode=telegram.ParseMode.HTML)
+						sent_message.edit_text(Text.DONE)
 				else:
-					if download.is_downloadable(url):
-						userfile = user_cmd
-						raw_file = download.download(url, userfile)
+					sent_message.edit_text(Text.NOT_SUPPORTED,parse_mode=telegram.ParseMode.HTML)
+			else:
+				if download.is_downloadable(url):
+					size = download.check_filesize(url)
+					if size <= 10000:
+						filename = user_cmd
+						raw_file = download.download(url, filename)
 						if "ERROR" in raw_file:
 							sent_message.edit_text(Text.FAILED+raw_file, parse_mode=telegram.ParseMode.HTML)
 						else:
 							bot.send_chat_action(chat_id=update.message.chat_id, action=telegram.ChatAction.TYPING)
 							sent_message.edit_text(Text.UPLOADING_GD)
 							dwnld_url = upload.upload(raw_file)
-							size = (os.path.getsize(raw_file))/1048576
 							sent_message.edit_text(Text.DONE.format(raw_file, size, dwnld_url),parse_mode=telegram.ParseMode.HTML)
 							os.remove(raw_file)
 					else:
-						sent_message.edit_text(Text.ISNOT_DOWNLOADABLE,parse_mode=telegram.ParseMode.HTML)
-			else:
-				if download.is_downloadable(url):
+						sent_message.edit_text(Text.MAXLIMITEXCEEDED)
+				else:
+					sent_message.edit_text(Text.ISNOT_DOWNLOADABLE,parse_mode=telegram.ParseMode.HTML)
+		else:
+			if download.is_downloadable(url):
+				size = download.check_filesize(url)/1048576
+				if size <= 10000:
 					raw_file = download.download(url, None)
 					bot.send_chat_action(chat_id=update.message.chat_id, action=telegram.ChatAction.TYPING)
 					sent_message.edit_text(Text.UPLOADING_GD)
 					dwnld_url = upload.upload(raw_file)
-					size = (os.path.getsize(raw_file))/1048576
 					sent_message.edit_text(Text.DONE.format(raw_file, size, dwnld_url),parse_mode=telegram.ParseMode.HTML)
-			 		os.remove(raw_file)
+			 		os remove(raw_file)
 				else:
-					sent_message.edit_text(Text.ISNOT_DOWNLOADABLE,parse_mode=telegram.ParseMode.HTML)
-		elif("help" not in url and "start" not in url and "broadcast" not in url and "donate" not in url and "add_user" not in url and "revoke_user" not in url):
-			bot.send_chat_action(chat_id=update.message.chat_id, action=telegram.ChatAction.TYPING)
-			time.sleep(1)
-			bot.send_message(chat_id=update.message.chat_id, text=Text.RETARD)
-	
-def 
-        main():
+					sent_message.edit_text(Text.MAXLIMITEXCEEDED)
+			else:
+				sent_message.edit_text(Text.ISNOT_DOWNLOADABLE,parse_mode=telegram.ParseMode.HTML)
+	elif("help" not in url and "start" not in url and "broadcast" not in url and "donate" not in url and "add_user" not in url and "revoke_user" not in url):
+		bot.send_chat_action(chat_id=update.message.chat_id, action=telegram.ChatAction.TYPING)
+		time.sleep(1)
+		sent_message.edit_text(Text.RETARD)
+
+def main():
 	updater = Updater(token=TOKEN, workers = 8)
 	dispatcher = updater.dispatcher
 	start_cmd = CommandHandler("start" , start)
 	help_cmd = CommandHandler("help" , help)
+	donate_cmd = CommandHandler("donate" , donate)
 	dispatcher.add_handler(start_cmd)
 	dispatcher.add_handler(help_cmd)
+	dispatcher.add_handler(donate_cmd)
 	if ADMIN_MODULE:
 		extras.add_extra_commands(dispatcher)
 	else:
